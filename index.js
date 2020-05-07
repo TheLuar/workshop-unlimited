286bc06b41862419d67b14a3f03fc76a388f95c6
TheLuar <58670652+TheLuar@users.noreply.github.com>
2020-05-07 19:09:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-'use strict'
-
-
 const express = require('express');
 const http = require('http');
-// const socketio = require('socket.io');
+const socketio = require('socket.io');
 
 
-const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
-// const io = socketio(server);
+const io = socketio(server);
 
 
-app.use(express.static('public'))
-server.listen(PORT)
+app.use(express.static('public'));
+server.listen(process.env.PORT || 3000);
+
+io.on('connection', socket => {
+	console.log(socket.id, 'has connected')
+});
